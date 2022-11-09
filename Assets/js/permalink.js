670a6f8e81edc7cfe8abad6a55aa7579b073448c
aljawaid <jawaid_imran@hotmail.com>
2022-11-09 01:09:59 +0000

    Added alt code icons for better user experience

--- Assets/js/permalink.js
+++ Assets/js/permalink.js
@@ -2,8 +2,8 @@ document.addEventListener("click", function (e) {
     if (e.target.id === "permalink-link") {
         e.preventDefault();
         navigator.clipboard.writeText(e.target.href)
-            .then(() => alert("Copied!"))
-            .catch((reason) => alert("Failed to copy: " + reason));
+            .then(() => alert("✓ Copied"))
+            .catch((reason) => alert("✘ Failed to copy: " + reason));
 
         return false;
     }
